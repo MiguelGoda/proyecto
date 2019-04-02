743d9b872fcbea4e8e09983b756ec46c57b12c61
Miguel Goda <miguel_g14@hotmail.com>
2019-04-02 01:27:00 -0400

    agregado combo box de memos

--- src/routes/memos.js
+++ src/routes/memos.js
@@ -7,11 +7,18 @@ router.get("/add", async (req, res) => {
   const tipo_memos = await pool.query(
     "select id_tipo_memo as tipo_memo_id_tipo_memo, tipo_memo from tipo_memo; "
   );
-  res.render("memos/add", { tipo_memos });
+  const persona = await pool.query(
+    "select id_persona as persona_id_persona, ap_paterno, ap_materno, nombres from persona;"
+  );
+
+  console.log(tipo_memos);
+  console.log(persona);
+
+  res.render("memos/add", { tipo_memos, persona });
 });
 
-router.post("/add", async (req, res) => {
-    const {tipo_memo_id_tipo_memo, nro_memo, descripcion_memo, fecha_memo, }
-})
+// router.post("/add", async (req, res) => {
+//     const {tipo_memo_id_tipo_memo, nro_memo, descripcion_memo, fecha_memo, }
+// })
 
 module.exports = router;
