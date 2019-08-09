b5f6bcf4b69f7db4fb72c70dbafd0187deca194d
Miguel Goda <miguel_g14@hotmail.com>
2019-08-09 18:27:46 -0400

    revirtiendo cambios

--- src/routes/direcciones.js
+++ src/routes/direcciones.js
@@ -3,18 +3,16 @@ const router = express.Router();
 
 const pool = require("../database");
 
-// router.get("/add", (req, res) => {
-//   res.render("direcciones/add");
-// });
+router.get("/add", (req, res) => {
+  res.render("direcciones/add");
+});
 
-router.post("/", async (req, res) => {
+router.post("/add", async (req, res) => {
   const { nombre_direccion, descripcion_direccion } = req.body;
   const newDireccion = {
     nombre_direccion,
     descripcion_direccion
   };
-  console.log(newDireccion);
-
   await pool.query("INSERT INTO direcciones set ?", [newDireccion]);
   req.flash("success", "La Direccion adicciono Correctamente");
   res.redirect("/direcciones");
@@ -22,8 +20,7 @@ router.post("/", async (req, res) => {
 
 router.get("/", async (req, res) => {
   const direcciones = await pool.query("SELECT * FROM direcciones");
-  const bien = true;
-  res.render("direcciones/list", { direcciones, bien });
+  res.render("direcciones/list", { direcciones });
 });
 
 router.get("/delete/:id", async (req, res) => {
@@ -33,7 +30,7 @@ router.get("/delete/:id", async (req, res) => {
   res.redirect("/direcciones");
 });
 
-router.get("/:id", async (req, res) => {
+router.get("/edit/:id", async (req, res) => {
   const { id } = req.params;
   const direcciones = await pool.query(
     "SELECT * FROM direcciones WHERE ID_DIRECCION = ?",
@@ -42,7 +39,7 @@ router.get("/:id", async (req, res) => {
   res.render("direcciones/edit", { direcciones: direcciones[0] });
 });
 
-router.put("/:id", async (req, res) => {
+router.post("/edit/:id", async (req, res) => {
   const { id } = req.params;
   const { nombre_direccion, descripcion_direccion } = req.body;
   const newDireccion = {
@@ -54,7 +51,7 @@ router.put("/:id", async (req, res) => {
     id
   ]);
   req.flash("success", "Direccion editada Correctamente");
-  res.redirect("/");
+  res.redirect("/direcciones");
 });
 
 module.exports = router;
